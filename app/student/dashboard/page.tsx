78b634e2d5c6595d14e86096833a1be8b38b4430
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-08-09 21:56:59 +0545

    feat: add StudentDashboardPage component for student dashboard interface

---
+++ app/student/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import StudentDashboardPage from '@/src/components/StudentProfile/Dashboard'
+import React from 'react'
+
+const page = () => {
+    return <StudentDashboardPage />
+}
+
+export default page
