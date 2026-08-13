360eb68b0c063ac2bd25e1dce6859367293636e6
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-08-13 21:46:49 +0545

    feat: http response dtos

--- src/types/http.ts
+++ src/types/http.ts
@@ -3,4 +3,14 @@ export interface ApiResponse<T> {
     success: boolean;
     message: string;
     data: T;
+    pagination?: PaginationMeta;
+
+}
+
+
+export interface PaginationMeta {
+    page: number;
+    page_size: number;
+    total_pages: number;
+    total_records: number;
 }
