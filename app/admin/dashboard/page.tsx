acca82d15eaf4bd4dddf1454475c128a9453208e
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-08-14 22:43:57 +0545

    initial implementation of admin dashboard, only includes org verification actions(verify,reject)

---
+++ app/admin/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { OrganizationVerificationsListPage } from '@/src/components/Admin/comp'
+import React from 'react'
+
+const page = () => {
+    return <OrganizationVerificationsListPage />
+}
+
+export default page
